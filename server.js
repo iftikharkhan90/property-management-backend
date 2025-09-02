73db38e31b00b81e2993e7332c88e8fd63ac2378
Usama-Asghar445 <usamaasghar7426>
2025-09-02 04:44:16 -0700

    add cors

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const mongoose = require("mongoose");
 const mainRoutes = require("./api-routes/index");
 const { creatUser } = require("./Module/user/controller");
 require("dotenv").config();
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin:"*",
-  methods:["GET","POST","PUT","PATCH","DELETE"],
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 const connection = process.env.DB_CONNECTION;
-mongoose
-  .connect(connection)
-  .then(async () => {
-    await creatUser();
-  })
+mongoose.connect(connection).then(async () => {
+  await creatUser();
+});
 const Api = process.env.MAIN_API_ROUTE;
 app.use(Api, mainRoutes);
 
 const Port = process.env.PORT;
-app.listen(Port,);
+app.listen(Port);
